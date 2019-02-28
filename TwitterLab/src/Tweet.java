8939bc85e5a40c79c5156a5764786ee1dc82af3b
cteckholm <eckholmc5346@my.uwstout.edu>
2019-02-28 15:50:55 -0600

    Update TwitterLab/src/Tweet.java

--- TwitterLab/src/Tweet.java
+++ TwitterLab/src/Tweet.java
@@ -1,4 +1,16 @@
-
+/**
+ * The purpose of this class is to encapsulate a tweet
+ * @author cteckholm
+ *
+ */
+import java.util.*;
+import twitter4j.GeoLocation;
 public class Tweet {
+	private String user, text;
+	private GeoLocation location;
+	private Date date;
+	private double sentiment;
+	
+	
 
 }
